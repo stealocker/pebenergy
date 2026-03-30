ed7293cb4e23585c0475839b847b55fb30909bd3
stealocker <lea@leastocker.design>
2026-03-30 20:31:51 +0200

    edit contact form text

--- blocks/lazyblock-kontakt/block.php
+++ blocks/lazyblock-kontakt/block.php
@@ -1,18 +1,18 @@
 <section class="contact grid">
     <h2><?php echo esc_html($attributes['headline']); ?></h2>
     <form id="contact-form" class="contact__form" action="#">
-        <p>Hi, mein Name ist <label for="name"></label><input type="text" id="name" name="name" required> und ihr erreicht mich unter<label for="email"></label>
-            <input type="email" id="email" name="email">. Ich komme mit folgendem Anliegen zu euch:<label for="inquiry"></label>
+        <p>Hallo, mein Name ist <label for="name"></label><input type="text" id="name" name="name" required> und Sie erreichen mich unter<label for="email"></label>
+            <input type="email" id="email" name="email">. Ich komme mit folgendem Anliegen zu Ihnen:<label for="inquiry"></label>
             <span class="select"><select id="inquiry" name="inquiry">
                 <option value="general">allgemeine Frage</option>
                 <option value="interest">ich habe Interesse</option>
                 <option value="existingBuilding">EMS im Bestandsgebäude</option>
                 <option value="newBuilding">EMS im Neubau</option>
-            </select></span>. Vielleicht könnt ihr mir hierbei helfen:
+            </select></span>. Vielleicht können Sie mir hierbei helfen:
         </p>
         <label for="message"></label>
         <textarea id="message" name="message" rows="5"></textarea>
-        <div class="dsgvo-wrapper"><label class="dsgvo" for="dsgvo">Bevor ihr fragt, ja ich habe die <a href="<?php echo esc_url($attributes['link']); ?>" tabindex="0">Datenschutzerklärung</a> gelesen und bin mit der Verarbeitung meiner personenbezogenen Daten einverstanden, deshalb setze ich hier mein Häkchen: <input type="checkbox" id="dsgvo" name="dsgvo" required tabindex="0"><span class="checkmark"></span></label></div>
+        <div class="dsgvo-wrapper"><label class="dsgvo" for="dsgvo">Bevor Sie fragen, ja ich habe die <a href="<?php echo esc_url($attributes['link']); ?>" tabindex="0">Datenschutzerklärung</a> gelesen und bin mit der Verarbeitung meiner personenbezogenen Daten einverstanden, deshalb setze ich hier mein Häkchen: <input type="checkbox" id="dsgvo" name="dsgvo" required tabindex="0"><span class="checkmark"></span></label></div>
         <button class="button--primary" type="submit" tabindex="0">Senden</button>
     </form>
 </section>
